9f59ae94f2eb20cff7415a089dc9a40866ef21de
Mriganka <me.mrig@amazon.com>
2016-06-19 16:44:31 +0530

    Laid out digits

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel?
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        let currentDisplay = display!.text!
+        
+        if currentDisplay != "0" {
+            display!.text = currentDisplay + digit
+        } else {
+            display!.text = digit
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
